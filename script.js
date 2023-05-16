6f77d9988e9b0fd6c624d82a20374db50b0ad609
Heavenpatel5114 <113304844+Heavenpatel5114@users.noreply.github.com>
2023-05-16 10:33:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+let evaluatedText = document.querySelector("#evaluatedText");
+let letterCount = document.querySelector("#letterCount");
+
+evaluatedText.addEventListener("input", function() {
+  letterCount.textContent = evaluatedText.value.length;
+});
